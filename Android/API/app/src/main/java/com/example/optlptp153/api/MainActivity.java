54767a35f4b4bec929e44e9c3ca0c49d41e71391
Anwesa Halder <anwesa.halder@optimizeitsystems.com>
2017-09-21 14:48:18 +0530

    API Project

---
+++ Android/API/app/src/main/java/com/example/optlptp153/api/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.optlptp153.api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
